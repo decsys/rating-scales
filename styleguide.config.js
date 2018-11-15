e0e423fdb7105e256e5e9033f3eb53765640186e
Jonathan Couldridge <jon@jon.gg>
2018-11-15 10:52:22 +0000

    question is in, frame and question now use prop types and defaults, switched jsdoc to react-styleguidist

---
+++ styleguide.config.js
@@ -0,0 +1,28 @@
+// const path = require("path");
+// const pkg = require("./package.json");
+
+module.exports = {
+  styleguideDir: "docs",
+  title: "Decsys Rating Scales",
+
+  // TODO fix final import pathing
+  // getComponentPathLine(componentPath) {
+  //   const name = path.basename(componentPath, ".js");
+
+  //   const dir = path.dirname(componentPath).replace(/src[\\\/]/, `${pkg.name}/`);
+  //   return `import ${name} from '${dir}';`;
+  // },
+
+  pagePerSection: true,
+  sections: [
+    {
+      name: "README",
+      content: "./README.md"
+    },
+    {
+      name: "Core Components",
+      content: "src/core/overview.md",
+      components: ["src/core/*.js"]
+    }
+  ]
+};
