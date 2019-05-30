ae53adc0249d7e66c9fcb4814f9c76f7d5d99e46
Jonathan Couldridge <jonathan.couldridge@nottingham.ac.uk>
2019-05-30 13:32:27 +0100

    ScaleLabel functional

--- src/ellipse/components/ScaleLabel/BarLabel.js
+++ src/ellipse/components/ScaleLabel/BarLabel.js
@@ -1,6 +1,22 @@
 import styled from "styled-components";
 import { labelDistance } from "../../consts";
 
+export const propTypes = {
+  /** A valid CSS Color value for the label text */
+  labelColor: PropTypes.string,
+  /** A valid CSS Font Family value for any labels associated with this Radio component. */
+  fontFamily: PropTypes.string,
+  /** A valid CSS Font Size value for any labels associated with this Radio component. */
+  fontSize: PropTypes.string,
+  /** Vertical alignment of the label relative to its position */
+  yAlign: PropTypes.oneOf(["above", "center", "below"]),
+  /**
+   * The index of the position of the label relative to the Scale Bar.
+   * 0 - hard left, 1 - center, 2 - hard right
+   */
+  labelIndex: PropTypes.number
+};
+
 /** A label at a position */
 const BarLabel = styled.label`
   color: ${({ labelColor: x }) => x};
@@ -45,21 +61,7 @@ const BarLabel = styled.label`
   }};
 `;
 
-BarLabel.propTypes = {
-  /** A valid CSS Color value for the label text */
-  labelColor: PropTypes.string,
-  /** A valid CSS Font Family value for any labels associated with this Radio component. */
-  fontFamily: PropTypes.string,
-  /** A valid CSS Font Size value for any labels associated with this Radio component. */
-  fontSize: PropTypes.string,
-  /** Vertical alignment of the label relative to its position */
-  yAlign: PropTypes.oneOf(["above", "center", "below"]),
-  /**
-   * The index of the position of the label relative to the Scale Bar.
-   * 0 - hard left, 1 - center, 2 - hard right
-   */
-  labelIndex: PropTypes.number
-};
+BarLabel.propTypes = propTypes;
 
 BarLabel.defaultProps = {
   labelColor: "black",
--- src/ellipse/components/ScaleLabel/ScaleLabel.js
+++ src/ellipse/components/ScaleLabel/ScaleLabel.js
@@ -1,36 +1,17 @@
 import React from "react";
 import PropTypes from "prop-types";
+import BarLabel, { propTypes } from "./BarLabel";
 
-/** A positionable label for a ScaleBar */
-export default class ScaleLabel extends React.Component {
-  static propTypes = {
-    /** The actual label text */
-    value: PropTypes.string.isRequired,
+const ScaleLabel = ({ value, ...p }) => (
+  <LabelPoint>
+    <BarLabel {...p}>{value}</BarLabel>
+  </LabelPoint>
+);
 
-    /** A valid CSS Color value for the label text */
-    labelColor: PropTypes.string,
+ScaleLabel.propTypes = {
+  /** The actual label text */
+  value: PropTypes.string.isRequired,
+  ...propTypes
+};
 
-    /** A valid CSS Font Family value for any labels associated with this Radio component. */
-    fontFamily: PropTypes.string,
-
-    /** A valid CSS Font Size value for any labels associated with this Radio component. */
-    fontSize: PropTypes.string,
-
-    /** Vertical alignment of the label relative to its position */
-    yAlign: PropTypes.oneOf(["above", "center", "below"]),
-
-    /**
-     * The index of the position of the label relative to the Scale Bar.
-     * 0 - hard left, 1 - center, 2 - hard right
-     */
-    labelIndex: PropTypes.number
-  };
-
-  render() {
-    return (
-      <LabelPoint>
-        <BarLabel {...this.props}>{this.props.value}</BarLabel>
-      </LabelPoint>
-    );
-  }
-}
+export default ScaleLabel;
